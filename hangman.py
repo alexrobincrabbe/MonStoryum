dbacfe9cd52e4aea53e79caba8b4ca3c414b6c25
Alexander Crabbe <alienzen@hotmail.com>
2024-06-12 22:33:52 +0000

    feat: add hangman game function

---
+++ hangman.py
@@ -0,0 +1,35 @@
+import random as rnd
+
+def hangman():
+    win = False
+    guesses_remaining = 5
+    word_list = ["monster", "bunnies", "mouse","pirate", ]
+    word = rnd.choice(word_list)
+    number_of_letters=len(word)
+    show_word=""
+    for step in range(number_of_letters):
+        show_word+=" _"
+    print(show_word)
+    while win == False:
+        guess = input("guess a letter")
+        if guess in word:
+            index=0
+            for letter in word:
+                if guess == letter:
+                    show_word = show_word[:index*2+1] + letter + show_word[index*2 + 2:]
+                index+=1
+            print(show_word)
+            if show_word.replace(" ","") == word:
+                print("you win")
+                win = True
+                return win
+        else:
+            print("incorrect")
+            guesses_remaining-=1
+            if guesses_remaining == 0:
+                print("you lose")
+                return win
+            else:
+                print(f"you have {guesses_remaining} wrong guesses left")
+
+print(hangman())
