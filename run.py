2ba277a459cfa016acd5aad14b26949d2a801346
Alexander Crabbe <alienzen@hotmail.com>
2024-06-13 13:39:08 +0000

    feat: set up google sheets API

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
 import random as rnd
 import os
 from rich import print
@@ -8,6 +11,17 @@ pretty.install()
 from prettytable import PrettyTable
 import time
 import math
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Hall_of_fame')
 
 custom_theme= Theme({
     "features": "green",
