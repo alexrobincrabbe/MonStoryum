86efab1db4d006ee0fa8a95dee87433fd3dcc25b
Alexander Crabbe <alienzen@hotmail.com>
2024-06-16 20:30:27 +0000

    fix(bug): replace recursive main function call with while loop, refactor play again to function

--- run.py
+++ run.py
@@ -9,24 +9,33 @@ from game.create_game import create_rooms
 from game.play_turn import enter_room
 
 
-def main():
+def play_again_prompt():
     '''
-    main function
+    prompt the player to play again or exit the game
     '''
-    # create the game
-    rooms = create_rooms()
-    # start the game
-    room_number = 0
-    enter_room(rooms, room_number)
-    # end the game or restart
     while True:
         play_again = Prompt.ask(
             "[chartreuse4]play again?[/chartreuse4] (yes/no)")
         if play_again == "yes" or play_again == "y":
-            main()
+            return True
         elif play_again == "no" or play_again == "n":
             print('goodbye')
-            return
+            return False
+
+
+def main():
+    '''
+    main function to run the game
+    '''
+    play_again = True
+    while play_again is True:
+        # create the game
+        rooms = create_rooms()
+        # start the game
+        room_number = 0
+        enter_room(rooms, room_number)
+        # end the game or restart
+        play_again = play_again_prompt()
 
 
 if __name__ == "__main__":
